fb41ab8a30979b95ca9ca7b63f18a5ba2d6bd1a9
action0709 <action0709@mail.ru>
2023-03-06 17:14:24 +0400

    Задачи 1,2,3,4,5,6,7,8,9,10

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
@@ -58,6 +61,37 @@ public class Main {
         // Пишем код для задачи 7
         for (int i = 1; i <= 512; i=i*2) {
             System.out.println(i);
+        }
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        // Пишем код для задачи 8
+        int income=29000;
+        int savings=0;
+        for (int i = 1; i <= 12; i++) {
+            savings=savings+income;
+            System.out.println("Месяц "+i+", сумма накоплений равна "+savings+" рублей");
+        }
+    }
+       public static void task9() {
+        System.out.println("Задача 9");
+        // Пишем код для задачи 9
+        int income=29000;
+        int savings=0;
+        for (int i = 1; i <= 12; i++) {
+            savings=savings+savings/100;
+            savings=savings+income;
+             System.out.println("Месяц "+i+", сумма накоплений равна "+savings+" рублей");
+        }
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
+        // Пишем код для задачи 10
+int k=1;
+        for (int i = 1; i <= 10; i++) {
+            k=2*i;
+            System.out.println("2*"+i+"="+k);
+
         }
     }
 }
