51e70ab07a148647aa78b52182caffd30707192b
action0709 <action0709@mail.ru>
2023-03-06 16:15:15 +0400

    Задачи 1,2,3,4,5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        // Пишем код для задачи 2
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        // Пишем код для задачи 3
+        for (int i = 0; i < 17; i=i+2) {
+            System.out.println(i);
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        // Пишем код для задачи 4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        // Пишем код для задачи 5
+        for (int i = 1904; i <= 2096; i=i+4) {
+            System.out.println(i+ " год является високосным");
+        }
     }
 }
